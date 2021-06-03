5b05a4b50dc12abdab2128ca73b4da0513971df8
James Daugherty <James@Mac12-James-Daugherty.local>
2021-06-03 18:45:51 -0400

    updated index and lib

--- index.js
+++ index.js
@@ -4,39 +4,93 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-menu = () => {
+menu = () => {}
+    
 createManager = () => {
     inquirer.prompt([
         {
             type: "input",
             name: "name",
             message: "Enter the managers name",
-            // Validation here
         }
         {
             type: "input",
             name: "id",
             message: "What is the team manager's id?",
-            // Validation here
         }
         {
             type: "input",
             name: "email",
             message: "What is the team amanger's email?",
-            // Validation here
         }
         {
             type: "input",
             name: "officeNumber",
             message: "What is the team manager's office number?",
-            // Validation here
         }
     ]).then(({name, id, email, officeNumber}) => {
         const manager = new Manager(name, id, email, officeNumber);
         console.log(manager)
     });
 }
-createManager();
+//
+createEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the engineer's name",
+        }
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's id?",
+        }
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's primary email?",
+        }
+        {
+            type: "input",
+            name: "github",
+            message: "What is the team members GitHub username?",
+        }
+    ]).then(({name, id, email, github}) => {
+        const manager = new Engineer(name, id, email, github);
+        console.log(engineer)
+    });
 }
+
+//
+createIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the managers name",
+        }
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's id?",
+        }
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email?",
+        }
+        {
+            type: "input",
+            name: "school",
+            message: "Where did the intern attend school?",
+        }
+    ]).then(({name, id, email, school}) => {
+        const manager = new Intern (name, id, email, school);
+        console.log(manager)
+    });
+}
+createManager();
+
 
 Menu();
