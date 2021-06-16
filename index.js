9f6f8bdb817fca721ac5e92a687c6d863bbcd273
James Daugherty <James@Mac12-James-Daugherty.local>
2021-06-16 08:21:03 -0400

    changed index.js

--- index.js
+++ index.js
@@ -4,42 +4,58 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+
+
+// createManager = () => {
+//     inquirer
+//     .prompt([
+//         {
+//             type: "input",
+//             name: "name",
+//             message: "Enter the managers name",
+//         },
+//         {
+//             type: "input",
+//             name: "id",
+//             message: "What is the team manager's id?",
+//         },
+//         {
+//             type: "input",
+//             name: "email",
+//             message: "What is the team manager's email?",
+//         },
+//         {
+//             type: "input",
+//             name: "officeNumber",
+//             message: "What is the team manager's office number?",
+//         }
+//     ]).then(({name, id, email, officeNumber}) => {
+//         const manager = new Manager(name, id, email, officeNumber);
+//         console.log(manager)
+//     });
+// }
+// createManager ();
+
+//Menu boilerplate from inquirer docs
 menu = () => {
-
+    inquirer
+  .prompt([
+    {type: "list",
+    name: "answers",
+    message: "Select the type of team member you'd like to create.",
+    choices: ["Engineer", "Intern"],
+    }])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    if (answers.choices === "Engineer") {
+        createEngineer();
+    } else createIntern();
+  })
 }
 
-createManager = () => {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the managers name",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is the team manager's id?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is the team manager's email?",
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "What is the team manager's office number?",
-        }
-    ]).then(({name, id, email, officeNumber}) => {
-        const manager = new Manager(name, id, email, officeNumber);
-        console.log(manager)
-    });
-}
-
-createManager ();
-//
 createEngineer = () => {
-    inquirer.prompt([
+    inquirer
+    .prompt([
         {
             type: "input",
             name: "name",
@@ -64,11 +80,11 @@ createEngineer = () => {
         const engineer = new Engineer(name, id, email, github);
         console.log(engineer)
     });
-}
-createEngineer();
+
 //
 createIntern = () => {
-    inquirer.prompt([
+    inquirer
+    .prompt([
         {
             type: "input",
             name: "name",
@@ -93,7 +109,16 @@ createIntern = () => {
     ]).then(({name, id, email, school}) => {
         const intern = new Intern (name, id, email, school);
         console.log(intern)
-    });
-}
-createIntern();
+    })}}
 
+
+    // Copied over from readme generator
+    // function writeToFile(fileName, data) {
+    //     fs.writeFile(fileName, data, (err) => {
+    //       if (err) {
+    //         return console.log(err);
+    //       }
+      
+    //       console.log("Your README.md has been successfully generated!");
+    //     });
+    //   }
